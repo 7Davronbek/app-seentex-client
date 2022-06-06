d2869a1910278ec8a36be57b62f048bff8e67672
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-06 15:28:01 +0500

    seentex-app

---
+++ src/components/SocialFixed.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SocialFixed = () => {
+    return (
+        <>
+
+            <div className="socialfixed">
+                <div className="net">
+                    <a href="https://t.me/+LHavaShLtpxmYjZi"><i className="fab fa-telegram net_1"></i></a>
+                    <a href="!#"><i className="fab fa-facebook net_1 "></i></a>
+                    <a href="https://instagram.com/sontexnika_elektrotovari_?utm_medium=copy_link">
+                        <i className="fab fa-instagram net_1"></i></a>
+                    <a href="!#"><i className="fab fa-youtube-square net_1 "></i></a>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default SocialFixed
