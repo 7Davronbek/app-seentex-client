3ddc7e88e77d93151b714bb5f6a141b1cb7bf50c
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-06-16 14:56:06 +0500

    seentex-app

--- src/locales/UZ.js
+++ src/locales/UZ.js
@@ -3,4 +3,22 @@ export const uz = {
     navbar2: "Biz haqimizda",
     navbar3: "Tovarlar",
     navbar4: "Aloqa",
+
+    aboutUs: "SEENTEX KOMPANIYAMIZ HAQIDA",
+    aboutUsP: ` "SEENTEX" гроуп образовалась в 2022 году и поставляет сантехнику по всему Узбекистану под брендом "SEENTEX". На основании договоров с самыми известными предприятиями КНР поставляет нашему народу качественную сантехнику. Одной из основных целей компании является обеспечение рынка качественными товарами. Высшая цель –F ориентированно на качество. В настоящее время, благодаря спросу нашего народа, мы начали предлагать различные модели смесителей и котлов в различных дизайнах. Стратегия компании заключается в расширении ассортимента сантехнической продукции и удовлетворении населения путем обеспечения его качественными товарами. `,
+
+    benefist: "BIZNING AFZALLIKLAR",
+    benefist1: "TOVAR NARXI VA SIFATNING OPTIMAL MUNOLOQATLIGI.",
+    benefist2: "100% SIFATLI VA XAVFSIZ TOVAR.",
+    benefist3: "XALQARO KOMPANALAR BIZ BILAN HAMKORLIK",
+    benefist4: "SODIQ MIJOZLARIMIZ UCHUN SOVG'ALAR BOR",
+
+    whyWe: "Nega aynan biz?",
+    whyWeP: "Bizning jamoamiz loyihalarni noldan boshlab va kalit taslim asosida amalga oshiradi. Biz barcha xizmatlarimizga to'liq kafolat beramiz.",
+    garantiya: "Kafolat",
+    garantiyaP: "Bizning jamoamiz loyihalarni noldan boshlab va kalit taslim asosida amalga oshiradi. Biz barcha xizmatlarimizga to'liq kafolat beramiz.",
+
+    ourAddress: "Bizning manzil",
+    address: " Optom - O'rikzor Stroy shahar 3 blok 10/61.",
+    galereya: "Galereya",
 }
